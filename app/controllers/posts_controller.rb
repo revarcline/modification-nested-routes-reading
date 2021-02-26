45e544332c4785bcb21d3562186dac19234f14bc
Alex Cline <rev.a.r.cline@gmail.com>
2021-02-26 17:01:47 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,26 @@
 class PostsController < ApplicationController
-
   def index
-    if params[:author_id]
-      @posts = Author.find(params[:author_id]).posts
-    else
-      @posts = Post.all
-    end
+    @posts = if params[:author_id]
+               Author.find(params[:author_id]).posts
+             else
+               Post.all
+             end
   end
 
   def show
-    if params[:author_id]
-      @post = Author.find(params[:author_id]).posts.find(params[:id])
-    else
-      @post = Post.find(params[:id])
-    end
+    @post = if params[:author_id]
+              Author.find(params[:author_id]).posts.find(params[:id])
+            else
+              Post.find(params[:id])
+            end
   end
 
   def new
-    @post = Post.new
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: 'Author not found.'
+    else
+      @post = Post.new(author_id: params[:author_id])
+    end
   end
 
   def create
@@ -33,12 +36,22 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    if params[:author_id]
+      author = Author.find_by(id: params[:author_id])
+      if author.nil?
+        redirect_to authors_path, alert: 'Author not found.'
+      else
+        @post.author.posts.find_by(id: params[:id])
+        redirect_to author_posts_path(author), alert: 'Post not found.' if @post.nil?
+      end
+    else
+      @post = Post.find(params[:id])
+    end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
